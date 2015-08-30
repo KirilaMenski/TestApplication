bae318f9e33ef9c2f7d3249ba18e2db59c579ec9
Mensky <bv_1514@mail.ru>
2015-08-30 22:01:28 +0300

    Converting servlet

---
+++ TestWebApplication/src/main/java/by/expertsoft/test/controllers/AddUsersController.java
@@ -0,0 +1,39 @@
+package by.expertsoft.test.controllers;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import by.expertsoft.test.action.ReaderCSV;
+
+@WebServlet("/addUsers")
+public class AddUsersController extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+	}
+	
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		ReaderCSV readerCSV = new ReaderCSV();
+		
+		String title = request.getParameter("title");
+		
+		readerCSV.convertToBD(title);
+		
+//		RequestDispatcher dispatcher = request.getRequestDispatcher("/listPage");
+//		dispatcher.forward(request, response);
+		response.sendRedirect("listPage");
+
+	}
+
+}
