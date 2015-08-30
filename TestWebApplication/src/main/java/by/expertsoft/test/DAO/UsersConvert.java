7b34b2fc468f0c2f1a4246c07922dca4f6e9e38d
Mensky <bv_1514@mail.ru>
2015-08-30 22:03:47 +0300

    DAO for converting

---
+++ TestWebApplication/src/main/java/by/expertsoft/test/DAO/UsersConvert.java
@@ -0,0 +1,13 @@
+package by.expertsoft.test.DAO;
+
+import java.sql.SQLException;
+
+import by.expertsoft.test.entity.User;
+
+public interface UsersConvert {
+	
+	public void convUsers(User user) throws SQLException;
+	
+	public void editUsers(User user) throws SQLException;
+
+}
