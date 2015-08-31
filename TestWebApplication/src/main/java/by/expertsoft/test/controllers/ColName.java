c8232dc119daaba16f9283e15a35816596b7912b
Mensky <bv_1514@mail.ru>
2015-08-31 22:07:35 +0300

    Class for sorting by column name

---
+++ TestWebApplication/src/main/java/by/expertsoft/test/controllers/ColName.java
@@ -0,0 +1,24 @@
+package by.expertsoft.test.controllers;
+
+public class ColName {
+	
+	private String colName;
+	
+	public ColName(){
+		
+	}
+	
+	public ColName(String colName){
+		this.colName = colName;
+	}
+
+	public String getColName() {
+		return colName;
+	}
+
+	public void setColName(String colName) {
+		this.colName = colName;
+	}
+	
+	
+}
